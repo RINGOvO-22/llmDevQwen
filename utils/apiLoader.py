526cf1547ceb6ccbb4035acb2173a20032f4c1b3
RINGOvO-22 <ddd3175930238@163.com>
2025-09-02 20:25:37 +0800

    hello_qwen

--- utils/apiLoader.py
+++ utils/apiLoader.py
@@ -1,18 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-def load_item():
+def load_key():
     # Load environment variables from .env file
     load_dotenv()
 
     # Get environment variables
-    api_key = os.getenv("OPENAI_API_KEY")
+    api_key = os.getenv("ALI_API_KEY")
 
     # Check if environment variables are present
     if not api_key:
-        raise ValueError("Environment variables are missing.")
+        raise ValueError("Environment variables \"ALI_API_KEY\" are missing.")
 
     # Return environment variables as dictionary
-    return {
-        "api_key": api_key
-    }
+    return api_key
